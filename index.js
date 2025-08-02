6cf59dec148c05dbc42b9f73c45f6391c8555348
Kaspercito <74380308+kaspercito@users.noreply.github.com>
2025-08-02 15:24:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 const { DateTime } = require("luxon");
 const express = require("express");
 const axios = require("axios");
-const cheerio = require("cheerio");
 
 const CUEVANA_URL = "https://wv4.cuevana.online";
 const bot = new Client({
